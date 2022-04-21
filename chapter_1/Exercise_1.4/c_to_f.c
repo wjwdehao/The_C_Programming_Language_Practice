98f8f4c40643f697a217d41d039e2d0b5cb73f1b
Junwei Wu <junweiwu@amazon.com>
2022-04-21 14:48:57 -0700

    Added Exercise 1.3 to 1.5

---
+++ chapter_1/Exercise_1.4/c_to_f.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 100;
+    step = 10;
+
+    celsius = lower;
+
+    printf("The Celsius to Fahrenheit Conversion Table\n");
+    while (celsius <= upper) {
+        fahr = celsius*9.0/5.0+32.0;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+
+    return 0;
+}
