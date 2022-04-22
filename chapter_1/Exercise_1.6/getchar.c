c803327c50f56144daf5d34784c5335f607bdd74
Junwei Wu <junweiwu@amazon.com>
2022-04-22 15:42:59 -0700

    Added exercise 1.6 and 1.7

---
+++ chapter_1/Exercise_1.6/getchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+    printf("Test if getchar() != EOF is 0 or 1: %d", getchar() != EOF);
+    return 0;
+}
---
+++ chapter_1/Exercise_1.7/print_EOF.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+
+    printf("Printing EOF now: %d", EOF);
+
+    return 0;
+}
